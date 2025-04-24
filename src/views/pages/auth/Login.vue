6ef910272c490e5011a80e384bec84da090aed76
pires lufungula <pires00243@gmail.com>
2025-04-24 01:29:07 -0400

    ELGOPLAN-25 -  Ajouter department

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -39,7 +39,7 @@ const [remember, rememberAttrs] = defineField('remember');
 </script>
 
 <template>
-    <LangConfiguration />
+    <LangConfiguration is-login-page="true" />
     <div class="bg-surface-50 dark:bg-surface-950 flex flex-col items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
         <div class="flex flex-col items-center justify-center">
             <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
