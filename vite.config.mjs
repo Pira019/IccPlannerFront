a1cabd4a9cdd85eb7010111baba38fe4524dda5d
pires lufungula <pires00243@gmail.com>
2025-08-09 05:31:05 -0400

    CORR.

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,6 @@ import Components from 'unplugin-vue-components/vite';
 
 import { defineConfig } from 'vite';
 
-import fs from "fs";
 // https://vitejs.dev/config/
 export default defineConfig({
     optimizeDeps: {
@@ -24,12 +23,12 @@ export default defineConfig({
         }
 
     },
-    server:{
+   /* server:{
         https:{
             key : fs.readFileSync(new URL('localhost-key.pem', import.meta.url)),
             cert : fs.readFileSync(new URL('localhost.pem', import.meta.url)),
         },
         host : true
-     }
+     }*/
 
 });
