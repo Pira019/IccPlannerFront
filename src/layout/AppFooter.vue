3581b3c34b98b50c3832ebc8c016face4e2d5dcc
pires lufungula <pires00243@gmail.com>
2025-08-09 03:49:48 -0400

    ELGOPLAN-44 : - Refonte ajuste la couleur.

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -3,6 +3,6 @@
 <template>
     <div class="layout-footer">
         By
-        <a href="https://lfgl-tech.com/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Lflg-tech</a>
+        <a href="https://lfgl-tech.com/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Lfgl-tech</a>
     </div>
 </template>
