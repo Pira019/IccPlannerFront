2a30f3eb89d8648508baa1ec075afa65a438f327
pires lufungula <pires00243@gmail.com>
2026-01-14 20:49:06 -0500

    ELGOPLAN-17: Cal

--- src/views/program/EditProgram.vue
+++ src/views/program/EditProgram.vue
@@ -20,11 +20,13 @@
                     <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{ $field.error?.message }}</Message>
                 </FormField>
             </div>
-            <Message class="mb-5" v-if="errorReq != null" severity="error">{{ errorReq?.message }}</Message>
-
-            <div class="grid">
-                <div class="col-end-6 flex justify-end">
-                    <Button type="submit" :loading="loading" :label="$t('Save')" />
+            <div class="flex flex-col gap-2">
+                <ResponseComponent :error="errorReq" :successResponse="success && !errorReq" :showSuccessMessage="true" />
+            </div>
+            <div class="flex justify-end mt-5">
+                <div class="flex gap-2">
+                    <Button type="button" severity="danger" size="small" @click="closeDialog" outlined class="w-1/2" :label="$t('Cancel')" icon="pi pi-times" />
+                    <Button type="submit" size="small" :label="$t('Save')" :loading="isSubmitting" icon="pi pi-save" />
                 </div>
             </div>
         </Form>
@@ -36,15 +38,14 @@ import ProgramService from '@/service/ProgramService';
 import { useHandleAsyncError } from '@/utils/handleAsyncError';
 import { zodResolver } from '@primevue/forms/resolvers/zod';
 import { ref } from 'vue';
-import { useI18n } from 'vue-i18n';
 import { z } from 'zod';
 
-const { t } = useI18n();
-const emit = defineEmits(['saved']);
+const emit = defineEmits(['saved', 'closeModal']);
 const { handleAsyncError } = useHandleAsyncError();
 
 const loading = ref(false);
 const errorReq = ref(null);
+const success = ref(false);
 
 const resolver = zodResolver(
     z.object({
@@ -60,10 +61,14 @@ const onFormSubmit = async ({ valid, values }) => {
     }
 };
 
+function closeDialog() {
+    emit('closeDialog');
+}
+
 ///
 async function editPrg(newPrg) {
     const { error, result } = await handleAsyncError(
-        () => ProgramService.addPrg(newPrg), 
+        () => ProgramService.addPrg(newPrg),
         (val) => (loading.value = val),
         true,
         'msgAddPrg'
@@ -71,6 +76,7 @@ async function editPrg(newPrg) {
     errorReq.value = error;
 
     if (result != null) {
+        success.value = true;
         emit('saved', {
             programId: result.programId,
             name: newPrg.name
